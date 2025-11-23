38fbc2ef2dfc2017ad430eac00ecf1a2d886d7d6
Will Watson <wdfwatson@gmail.com>
2025-11-23 16:41:14 -0400

    Fixed path generation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-use std::{collections::HashMap, iter};
+use std::{collections::HashMap};
 
 fn main() {
     let mut calculator = CollatzishMemoized::new(3, 5);
-    calculator.add_all_paths(1000000000);
-    println!("{:#?}",calculator.final_loop_map);
-    //println!("{:#?}",calculator.number_path_map);
+    let limit = 100000;
+    calculator.add_all_paths(limit);
+    let mut counts = HashMap::new();
+    for final_loop in calculator.final_loop_map.keys() {
+        counts.insert(final_loop.clone(), 0u64);
+        println!("{:?}",calculator.get_path(*final_loop));
+    }
+    let filtered_iter = calculator.number_path_map
+        .iter()
+        .filter(|(k,v)| **k <= limit)
+        .map(|(k,v)| v.clone().final_loop);
+    for final_loop in filtered_iter {
+        if let Some(count) = counts.get(&final_loop) {
+            counts.insert(final_loop, count+1);
+        }
+    }
+    println!("{:#?}",counts);
 }
 
 #[derive(Debug, Clone)]
@@ -48,7 +62,11 @@ impl CollatzishMemoized {
     }
 
     fn add_path(&mut self, starting_num: u64) -> NumberPath {
+        if let Some(path) = self.number_path_map.get(&starting_num) {
+            return path.clone();
+        }
         let mut path_list = vec![starting_num];
+        let mut new_loop = false;
         // keep going until we reach a known value of some kind
         let base_path = loop {
             let curr_num = path_list.last().expect("Should never be empty.").clone();
@@ -59,6 +77,7 @@ impl CollatzishMemoized {
             // need to check if a loop has been found
             if path_list.contains(&next_num) {
                 // we found a loop, so we need to add the loop
+                path_list = path_list.into_iter().take_while(|x| *x != next_num).collect();
                 break self.add_loop(next_num);
             }
             path_list.push(next_num);
@@ -74,6 +93,7 @@ impl CollatzishMemoized {
                     length_to_loop: curr_dist_from_loop 
                 }
             );
+            
         }
         return NumberPath { 
             start: starting_num, 
@@ -90,7 +110,7 @@ impl CollatzishMemoized {
         let mut min_num = starting_num;
         let mut curr_num = self.calc_next(starting_num);
         let mut loop_length = 1u64;
-        while curr_num != starting_num {
+        while curr_num != starting_num {     
             loop_length += 1;
             if curr_num < min_num {
                 min_num = curr_num;
@@ -119,9 +139,42 @@ impl CollatzishMemoized {
     }
 
     fn add_all_paths(&mut self, limit: u64) {
-        for i in 1..limit {
+        for i in 1..=limit {
             self.add_path(i);
         }
+    }
+
+    fn create_path_list(&self, number_path: NumberPath) -> Vec<u64> {
+        let mut path_list = Vec::new();
+        let mut curr_number_path = number_path;
+        while curr_number_path.length_to_loop > 0 {
+            path_list.push(curr_number_path.start);
+            curr_number_path = self.number_path_map
+                .get(&curr_number_path.next)
+                .expect("If path start exists, whole path should exist")
+                .clone();
+        }
+        let loop_start = curr_number_path.clone();
+        while loop_start.start != curr_number_path.next {
+            path_list.push(curr_number_path.start);
+            curr_number_path = self.number_path_map
+                .get(&curr_number_path.next)
+                .expect("If path start exists, whole path should exist")
+                .clone();
+        }
+        path_list.push(curr_number_path.start);
+        return path_list;
+    }
+
+    fn get_path(&self, starting_num: u64) -> Option<Vec<u64>> {
+        let number_path = self.number_path_map.get(&starting_num)?.clone();
+        return Some(self.create_path_list(number_path));
+    }
+
+    // calcs the path if it doesn't already exist
+    fn get_add_path(&mut self, starting_num: u64) -> Vec<u64> {
+        let number_path = self.add_path(starting_num);
+        return self.create_path_list(number_path);
     }
 
 }
